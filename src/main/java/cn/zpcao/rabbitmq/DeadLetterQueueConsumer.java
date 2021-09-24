1ef56e414d5f29222df7bb3765a376e36984972a
soushihou <pinocchio199604@gmail.com>
2021-09-24 18:32:40 +0800

    Initial commit

---
+++ src/main/java/cn/zpcao/rabbitmq/DeadLetterQueueConsumer.java
@@ -0,0 +1,27 @@
+package cn.zpcao.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * @author caozhipeng
+ * @title: DeadLetterQueueConsumer
+ * @projectName rabbitmq-springboot
+ * @description: TODO
+ * @date 2021/9/24 14:29
+ */
+@Slf4j
+@Component
+public class DeadLetterQueueConsumer {
+    @RabbitListener(queues = "QD")
+    public void receiveD(Message message, Channel channel) throws IOException {
+        String msg = new String(message.getBody());
+        log.info("当前时间：{},收到死信队列信息{}", new Date().toString(), msg);
+    }
+}
